b46697d747580c4bc7bfa1d1ca25bf3e6342c387
Piotr Bujok <p.bujok@gmail.com>
2017-04-12 19:59:50 +0200

    add test for has defined value

---
+++ src/Domain.Test/HasPropertyDefinedTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.WorkoutMerge;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.Test
+{
+    public class HasPropertyDefinedTests
+    {
+        [Fact]
+        public void TrackItemHasDefinedProperty_HasDefined_True()
+        {
+            var sut = new TrackItem(new DateTime(2017, 4, 14), new Coordiantes(1, 2), new Cadence(86), new Pulse(), new Altitude(), new Distance());
+            sut.HasPropertyDefined("Cadence").Should().BeTrue();
+        }
+
+        [Fact]
+        public void TrackItemHasDefinedProperty_NotDefined_False()
+        {
+            var sut = new TrackItem(new DateTime(2017, 4, 14), new Coordiantes(1, 2), new Cadence(86), new Pulse(), new Altitude(), new Distance());
+            sut.HasPropertyDefined("Pulse").Should().BeFalse();
+        }
+
+        [Fact]
+        public void SegmentHasDefinedProperty_Defined_True()
+        {
+            var item = new TrackItem(new DateTime(2017, 4, 14), new Coordiantes(1, 2), new Cadence(86), new Pulse(), new Altitude(), new Distance());
+            var sut = new Segment("Sport", new DateTime(2017, 4, 14), new List<TrackItem> { item }, new Calories(123), "High", TriggerMethodValue.Manual);
+            sut.HasPropertyDefined("Pulse").Should().BeFalse();
+        }
+
+        [Fact]
+        public void WorkoutHasDefinedProperty_Defined_True()
+        {
+            var item = new TrackItem(new DateTime(2017, 4, 14), new Coordiantes(1, 2), new Cadence(86), new Pulse(), new Altitude(), new Distance());
+            var segment = new Segment("Sport", new DateTime(2017, 4, 14), new List<TrackItem> { item }, new Calories(123), "High", TriggerMethodValue.Manual);
+            var sut = new Workout(new List<Segment>() { segment });
+            sut.HasPropertyDefined("Pulse").Should().BeFalse();
+        }
+    }
+}
